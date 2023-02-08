1e3cf1a19691ba9c4549726b87f347291ff8a6eb
marcosknudsen <marcosknu14@gmail.com>
2023-02-07 23:46:36 -0300

    Skip played matches

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ export default async function (id) {
     const pagina = await navegador.newPage();
     let table;
     await pagina.goto(`https://www.promiedos.com.ar/club=${id}`);
-    let response = await pagina.evaluate(()=>
+    let response = await pagina.evaluate(() =>
       Array.from(
         document.querySelector(".fixclub").children[0].children,
         function (e) {
-          var obj = {
-            day: e.children[0].innerText,
-            against: e.children[3].innerText,
-            matchN: e.children[1].innerText,
-            hoa: e.children[2].innerText=="L"?"H":"A",
-          };
-          return obj;
+          if (e.children[4].innerHTML == "-") {
+            var obj = {
+              day: e.children[0].innerText,
+              against: e.children[3].innerText,
+              matchN: e.children[1].innerText,
+              hoa: e.children[2].innerText == "L" ? "H" : "A",
+            };
+            return obj;
+          }
         }
       )
-    )
+    );
     response.shift();
     return response;
   } catch (e) {
