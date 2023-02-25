de0ce54e6a9e9a535086bf311e98bc6532cd2a34
Marcos Knudsen <marcosknu14@gmail.com>
2023-02-25 18:21:20 -0300

    today and yesterday results

--- index.js
+++ index.js
@@ -31,6 +31,72 @@ export async function getMatchesById(id) {
   }
 }
 
+export async function getTodayResults() {
+  try {
+    const navegador = await puppeteer.launch();
+    const pagina = await navegador.newPage();
+    await pagina.goto("https://www.promiedos.com.ar/");
+    let matches = await pagina.evaluate(() =>
+      Array.from(
+        document.querySelectorAll("tr[name=nvp]"),
+        function (e) {
+          if (e.children[0].classList[0] == "game-fin")
+          return {
+            localTeam:
+              e.children[1].children[e.children[1].children.length - 1]
+                .innerText,
+            localScore: e.children[2].innerText,
+            awayTeam:
+              e.children[4].children[e.children[4].children.length - 1]
+                .innerText,
+            awayScore: e.children[3].innerText,
+          };
+        }
+      )
+        .sort((a, b) => {
+          return parseInt(a.time) - parseInt(b.time);
+        })
+        .filter((d) => d != null)
+    );
+    return matches;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function getYesterdayResults(){
+  try {
+    const navegador = await puppeteer.launch();
+    const pagina = await navegador.newPage();
+    await pagina.goto("https://www.promiedos.com.ar/ayer");
+    let matches = await pagina.evaluate(() =>
+      Array.from(
+        document.querySelectorAll("tr[name=nvp]"),
+        function (e) {
+          if (e.children[0].classList[0] == "game-fin")
+          return {
+            localTeam:
+              e.children[1].children[e.children[1].children.length - 1]
+                .innerText,
+            localScore: e.children[2].innerText,
+            awayTeam:
+              e.children[4].children[e.children[4].children.length - 1]
+                .innerText,
+            awayScore: e.children[3].innerText,
+          };
+        }
+      )
+        .sort((a, b) => {
+          return parseInt(a.time) - parseInt(b.time);
+        })
+        .filter((d) => d != null)
+    );
+    return matches;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 export async function getMatchesToday() {
   try {
     const navegador = await puppeteer.launch();
